f0d8d94ca2e01d208dcf2755a60ae04d7013cf60
Gowtham <gowthaman@nfnlabs.com>
2021-07-30 02:08:48 +0530

    compose stable and hilt 2.38.1 update

---
+++ app/src/main/java/com/compose/template/di/AppModule.kt
@@ -0,0 +1,18 @@
+package com.compose.template.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+
+    @Singleton
+    @Provides
+    fun provideHttpClient(): Int {
+        return 1
+    }
+}
